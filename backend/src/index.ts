d4da3bfec1c59ffb3afcc147c8d64c9bdbd4ade8
Bongatoughy <lewiskyler@gmail.com>
2020-08-19 21:47:11 -0400

    [00004] Add express server to backend
    - Initialize express server and listen on env var PORT
    - Add dotenv

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,14 @@
-console.log("hi from drawzone");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+app.get("/", (_, res) => {
+  res.send("testing");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
