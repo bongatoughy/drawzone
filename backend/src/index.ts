717eb1f158bc96b87a01ad3e598dbacc6628bd2b
Bongatoughy <lewiskyler@gmail.com>
2020-08-20 01:09:15 -0400

    [00006] Backend create auth routes
    - Setup db and docker compose for dev
    - Create login and register route, service, controller

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,42 @@
 import express from "express";
+import { json } from "body-parser";
+import cors from "cors";
 import { config } from "dotenv";
+import { login, register } from "./services";
 
 config();
 
 const app = express();
 
-app.get("/", (_, res) => {
-  res.send("testing");
+app.use(cors());
+app.use(json());
+
+app.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await register(email, password);
+    res.send(user);
+  } catch (e) {
+    res
+      .status(400)
+      .send(
+        [
+          `Encountered an error, please try again. `,
+          `If error persists please use a different `,
+          `email address.`,
+        ].join("")
+      );
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await login(email, password);
+    res.send(user);
+  } catch (e) {
+    res.status(401).send(`Login attempt was unsuccessful. Please try again.`);
+  }
 });
 
 app.listen(process.env.PORT, () => {
