87520f42a94661046acd69bc78c4e0cbd4dced3e
Bongatoughy <lewiskyler@gmail.com>
2020-08-24 14:34:24 -0400

    [00012] Add drawings strokes and points to data model

--- backend/src/schema.ts
+++ backend/src/schema.ts
@@ -14,4 +14,33 @@ export const SCHEMA = {
       email           VARCHAR(255) UNIQUE NOT NULL
     )
   `,
+  DRAWINGS: `
+    CREATE TABLE IF NOT EXISTS
+    drawings (
+      id              BIGSERIAL PRIMARY KEY,
+      user_id         BIGSERIAL REFERENCES users(id),
+      created_at      TIMESTAMP NOT NULL,
+      completed_at    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      is_public       BOOLEAN DEFAULT FALSE
+    )
+  `,
+  STROKES: `
+    CREATE TABLE IF NOT EXISTS
+    strokes (
+      id              BIGSERIAL PRIMARY KEY,
+      color           VARCHAR(255) NOT NULL,
+      width           INTEGER DEFAULT 1,
+      brush           VARCHAR(255) NOT NULL,
+      drawing_id      BIGSERIAL REFERENCES drawings(id)
+    )
+  `,
+  POINTS: `
+    CREATE TABLE IF NOT EXISTS
+    points (
+      id              BIGSERIAL PRIMARY KEY,
+      x               INTEGER NOT NULL,
+      y               INTEGER NOT NULL,
+      stroke_id       BIGSERIAL REFERENCES strokes(id)
+    )
+  `,
 };
