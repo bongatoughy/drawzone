a3a19f2997bf4fff5d5ecda3f4359aaeebcb43f1
Bongatoughy <lewiskyler@gmail.com>
2020-08-24 14:32:41 -0400

    [00011] Add homepage content

--- frontend/src/home.tsx
+++ frontend/src/home.tsx
@@ -1,14 +1,175 @@
 import React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
+import Badge from "@material-ui/core/Badge";
+import Avatar from "@material-ui/core/Avatar";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
-import Slide from "@material-ui/core/Slide";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Toolbar from "@material-ui/core/Toolbar";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  drawer: {
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    [theme.breakpoints.up("sm")]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  badge: {
+    marginLeft: 1,
+  },
+  tabs: {
+    alignItems: "flex-start",
+    paddingLeft: 2,
+  },
+}));
 
 export const Home = ({ currentUser }) => {
-  return (
-    <Slide in={!!currentUser} direction="down">
-      <Box py={2} width={1} bgcolor="primary">
-        <Typography variant="h3">drawzone</Typography>
+  const classes = useStyles();
+  const theme = useTheme();
+
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [tabValue, setTabValue] = React.useState(0);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const handleTabChange = (_, newValue) => {
+    setTabValue(newValue);
+  };
+
+  const drawer = (
+    <Box>
+      <Box p={2}>
+        <Badge
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          badgeContent={4}
+          color="primary"
+          overlap="circle"
+        >
+          <Avatar alt="Travis Howard" />
+        </Badge>
+        <Box mt={2}>
+          <Typography>{currentUser}</Typography>
+        </Box>
       </Box>
-    </Slide>
+      <Divider />
+      <Box py={2}>
+        <Tabs
+          orientation="vertical"
+          variant="scrollable"
+          value={tabValue}
+          onChange={handleTabChange}
+          aria-label="Vertical tabs example"
+        >
+          <Tab classes={{ wrapper: classes.tabs }} label="main" />
+          <Tab classes={{ wrapper: classes.tabs }} label="create" />
+        </Tabs>
+      </Box>
+      <Divider />
+    </Box>
   );
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            drawzone
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+        </Typography>
+      </main>
+    </div>
+  );
+};
+
+Home.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
 };
--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -1,5 +1,8 @@
 export const ROUTES = {
-  REGISTER: `${process.env.HOST}/register`,
-  LOGIN: `${process.env.HOST}/login`,
-  REFRESH_TOKEN: `${process.env.HOST}/refresh_token`,
+  REGISTER: `/register`,
+  LOGIN: `/login`,
+  REFRESH_TOKEN: `/refresh_token`,
+  // REGISTER: `${process.env.HOST}/register`,
+  // LOGIN: `${process.env.HOST}/login`,
+  // REFRESH_TOKEN: `${process.env.HOST}/refresh_token`,
 };
